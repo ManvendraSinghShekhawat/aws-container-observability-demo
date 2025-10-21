8ecb01578e4c2d7d0c2ac64ab55239356d3720d7
Manvendra Singh Shekhawat <65719012+ManvendraSinghShekhawat@users.noreply.github.com>
2025-10-21 17:10:01 +0100

    Update enable-cloudwatch.sh

--- infra/monitoring/enable-cloudwatch.sh
+++ infra/monitoring/enable-cloudwatch.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+set -e
+AWS_REGION=${AWS_REGION:-eu-west-1}
+
+# Install aws-for-fluent-bit (sends logs to CloudWatch Logs)
+kubectl create namespace amazon-cloudwatch || true
+helm repo add aws-for-fluent-bit https://aws.github.io/eks-charts
+helm repo update
+
+helm upgrade --install aws-for-fluent-bit aws-for-fluent-bit/aws-for-fluent-bit \
+  --namespace amazon-cloudwatch \
+  --set cloudWatch.region=$AWS_REGION \
+  --set cloudWatch.logGroup=/aws/containerinsights/${CLUSTER_NAME}/application \
+  --set output.elasticsearch.enabled=false \
+  --set cloudWatch.createLogGroup=true \
+  --wait --timeout 10m
 
