1388c2dc509300dc1b55ec70cf8fee23d77a68ba
Manvendra Singh Shekhawat <65719012+ManvendraSinghShekhawat@users.noreply.github.com>
2025-10-21 17:10:29 +0100

    Update server.js

--- k8s/sample-app/server.js
+++ k8s/sample-app/server.js
@@ -1,1 +1,16 @@
+const http = require('http');
+const port = process.env.PORT || 8080;
+const handler = (req, res) => {
+  if (req.url === '/error') {
+    // simulate error
+    console.error("Simulated error!");
+    res.writeHead(500, {'Content-Type': 'text/plain'});
+    res.end('Simulated error\n');
+    return;
+  }
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from sample-app\n');
+};
+const server = http.createServer(handler);
+server.listen(port, () => console.log(`Server listening on ${port}`));
 
